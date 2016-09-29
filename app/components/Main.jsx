ab5548ff6a61c1a916bc5617642454a92ccde506
Nick Steuver <nbsteuv@gmail.com>
2016-09-29 05:27:14 -0400

    Added countdown form component and added container to restrain size

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -2,25 +2,17 @@ var React = require('react');
 
 var Nav = require('Nav');
 
-// var Main = React.createClass({
-//   render: function(){
-//     return (
-//       <div>
-//         <Nav />
-//         {this.props.children}
-//       </div>
-//
-//     );
-//   }
-// });
-
 var Main = (props) => {
   return (
     <div>
       <div>
         <div>
           <Nav />
-          {props.children}
+          <div className="row">
+            <div className="column small-centered medium-6 large-4">
+              {props.children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
