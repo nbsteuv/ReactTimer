c313f49ddd1b7c7c0056fb9abad245926230a210
Nick Steuver <nbsteuv@gmail.com>
2016-10-03 07:58:10 -0400

    Loading foundation through scss for more control over defaults

--- app/app.jsx
+++ app/app.jsx
@@ -7,7 +7,6 @@ var Countdown = require('Countdown');
 var Timer = require('Timer');
 
 //Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 //App.scss
